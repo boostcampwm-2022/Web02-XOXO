9ffc92d0b05ec7a4dc54d91a9c43ffb2566a34b7
jungmin <batay@soongsil.ac.kr>
2022-11-15 20:52:42 +0900

    chore : 프론트엔드 환경 설정

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+}
---
+++ frontend/src/components/MainComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainComponent = () => {
+  return (
+        <div>
+            <h1>컴포넌트!!!!!!!!!</h1>
+        </div>
+  )
+}
+
+export default MainComponent
