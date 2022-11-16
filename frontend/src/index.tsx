866a2ea9d39fc67970d00bf76a2392c3745dc0ab
GyuhyunBaek <edhz8888@gmail.com>
2022-11-16 15:28:51 +0900

    chore : 리엑트 라우팅 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import MainPage from '@src/pages/MainPage'
+import CreateFeed from '@src/pages/CreateFeed'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <MainPage />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/createfeed" element={<CreateFeed />} />
+    </Routes>
+  </BrowserRouter>
 )
