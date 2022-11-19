bdc023a1209ce348b9acd759e7428d4ba9eebb57
GyuhyunBaek <edhz8888@gmail.com>
2022-11-19 17:50:17 +0900

    feat : 임시적으로 posting 라우팅 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import MainPage from '@src/pages/MainPage'
 import CreateFeed from '@src/pages/CreateFeed'
+import Posting from '@src/pages/Posting'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -10,6 +11,7 @@ root.render(
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/createfeed" element={<CreateFeed />} />
+      <Route path="/posting" element={<Posting />} />
     </Routes>
   </BrowserRouter>
 )
