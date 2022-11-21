db383229ed2ad0748d1926007bed87794bc6f8aa
GyuhyunBaek <edhz8888@gmail.com>
2022-11-21 17:10:19 +0900

    feat : toastify 커스텀 완료

---
+++ frontend/src/components/Toast/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './styles.scss'
+const Toast = () => {
+  return (
+    <ToastContainer
+      position="bottom-center"
+      autoClose={1000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable={false}
+      pauseOnHover
+      theme="light"
+      closeButton={false}
+    />
+  )
+}
+
+export default Toast
--- frontend/src/pages/Feed/index.tsx
+++ frontend/src/pages/Feed/index.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './style.scss'
 import FeedPostingList from './FeedPostingList'
 import FeedProfile from './FeedProfile'
 import Header from '@components/Header'
+import { toast } from 'react-toastify'
+import Toast from '@src/components/Toast'
 
 const MainPage = () => {
-  // 테스트 데이터
+  useEffect(() => {
+    toast('게시물이 공개되기까지 14일 4시간 남았어요')
+  }, [])
   const feedInfo = { name: '부스트캠프 웹모바일 7기...' }
   return (
-      <div className='feed-page'>
-        <Header page='feed' text={feedInfo.name}/>
-        <div className='feed-body'>
-          <FeedProfile/>
-          <FeedPostingList/>
-        </div>
+    <div className="feed-page">
+      <Header page="feed" text={feedInfo.name} />
+      <div className="feed-body">
+        <FeedProfile />
+        <FeedPostingList />
+        <Toast />
       </div>
+    </div>
   )
 }
 
