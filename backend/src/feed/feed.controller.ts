f1d80cd5b017c6efc21961513c9f0b1d03d2b7ad
CHOIHYEONSEO <dwc06631@gmail.com>
2022-11-21 14:21:12 +0900

    feat: 피드 생성 api 리턴값인 피트 id 암호화 기능 추가

--- backend/src/feed/feed.controller.ts
+++ backend/src/feed/feed.controller.ts
@@ -3,6 +3,14 @@ import CreateFeedDto from './dto/create.feed.dto';
 import { FeedService } from './feed.service';
 import { Feed } from 'src/customDecorator/feed.decorator';
 import userIdDto from './dto/create.userId.dto';
+import {
+  createCipher,
+  createCipheriv,
+  createDecipheriv,
+  randomBytes,
+  scrypt,
+} from 'crypto';
+import { promisify } from 'util';
 
 @Controller('feed')
 export class FeedController {
@@ -15,6 +23,26 @@ export class FeedController {
   ) {
     const id = new userIdDto(userId);
     const feed = await this.feedService.createFeed(createFeedDto, id.userId);
-    return feed.id;
+
+    const iv = randomBytes(16);
+    const key = process.env.SECRET_KEY;
+
+    const cipher = createCipheriv('aes-256-cbc', key, iv);
+
+    const encryptedText = Buffer.concat([
+      cipher.update(feed.id.toString()),
+      cipher.final(),
+    ]).toString('hex');
+
+    const decipher = createDecipheriv('aes-256-cbc', key, iv);
+    const encrypt = Buffer.from(encryptedText, 'hex');
+    const decryptedText = Buffer.concat([
+      decipher.update(encrypt),
+      decipher.final(),
+    ]).toString();
+
+    console.log(encryptedText, decryptedText);
+
+    return encryptedText;
   }
 }
