d863a20ed720561bea9e672727da77f9ca273781
jungmin <batay@soongsil.ac.kr>
2022-11-21 11:15:07 +0900

    feat : 포스팅 작성 페이지 마크업

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Feed from '@src/pages/Feed'
 import CreateFeed from '@src/pages/CreateFeed'
-import SignIn from '@src/pages/SignIn'
-import Write from '@src/pages/Write'
+import Write from '@pages/Write'
+import SignIn from '@pages/SignIn'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/Feed" element={<Feed />} />
+      <Route path="/Write" element={<Write />} />
+      <Route path="/SignIn" element={<SignIn />} />
       <Route path="/createfeed" element={<CreateFeed />} />
       <Route path="/SignIn" element={<SignIn />} />
-      <Route path="/Write" element={<Write />} />
     </Routes>
   </BrowserRouter>
 )
