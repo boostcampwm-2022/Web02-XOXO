8a707fe9d7aec73e1951042d11c272f0161ac939
GyuhyunBaek <edhz8888@gmail.com>
2022-11-21 14:44:18 +0900

    fix : import 구문 통일

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Feed from '@src/pages/Feed'
-import CreateFeed from '@src/pages/CreateFeed'
+import Feed from '@pages/Feed'
+import CreateFeed from '@pages/CreateFeed'
 import Write from '@pages/Write'
 import SignIn from '@pages/SignIn'
-import Posting from '@src/pages/Posting'
-import Feeds from '@src/pages/Feeds'
+import Posting from '@pages/Posting'
+import Feeds from '@pages/Feeds'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
