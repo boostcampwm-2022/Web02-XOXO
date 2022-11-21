1630db951db42144227787131d405e3db8f1f543
CHOIHYEONSEO <dwc06631@gmail.com>
2022-11-21 14:42:06 +0900

    refactor: encrypt, decrypt 로직 untils 로  분리

--- backend/src/feed/feed.controller.ts
+++ backend/src/feed/feed.controller.ts
@@ -11,6 +11,7 @@ import {
   scrypt,
 } from 'crypto';
 import { promisify } from 'util';
+import { decrypt } from './feed.utils';
 
 @Controller('feed')
 export class FeedController {
@@ -22,27 +23,11 @@ export class FeedController {
     @Feed() createFeedDto: CreateFeedDto,
   ) {
     const id = new userIdDto(userId);
-    const feed = await this.feedService.createFeed(createFeedDto, id.userId);
+    const feedParam = await this.feedService.createFeed(
+      createFeedDto,
+      id.userId,
+    );
 
-    const iv = randomBytes(16);
-    const key = process.env.SECRET_KEY;
-
-    const cipher = createCipheriv('aes-256-cbc', key, iv);
-
-    const encryptedText = Buffer.concat([
-      cipher.update(feed.id.toString()),
-      cipher.final(),
-    ]).toString('hex');
-
-    const decipher = createDecipheriv('aes-256-cbc', key, iv);
-    const encrypt = Buffer.from(encryptedText, 'hex');
-    const decryptedText = Buffer.concat([
-      decipher.update(encrypt),
-      decipher.final(),
-    ]).toString();
-
-    console.log(encryptedText, decryptedText);
-
-    return encryptedText;
+    return feedParam;
   }
 }
