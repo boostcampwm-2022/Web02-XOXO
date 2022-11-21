eb7e115da90cd26f8e53761755e3e7857a600a2c
CHOIHYEONSEO <dwc06631@gmail.com>
2022-11-21 10:32:00 +0900

    feat: @feed custom decorator 생성

---
+++ backend/src/customDecorator/feed.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Feed = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const body = { ...ctx.switchToHttp().getRequest().body };
+    delete body.userId;
+    return body;
+  },
+);
