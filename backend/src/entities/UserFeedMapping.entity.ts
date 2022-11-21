2ee1a974a1f4a098790bd62dc25e5db45559313b
CHOIHYEONSEO <dwc06631@gmail.com>
2022-11-21 10:28:11 +0900

    refactor: userFeedMapping entity constraint 수정

--- backend/src/entities/UserFeedMapping.entity.ts
+++ backend/src/entities/UserFeedMapping.entity.ts
@@ -12,11 +12,15 @@ import User from './User.entity';
 
 @Entity({ schema: 'xoxo', name: 'user_feed_mapping' })
 export default class UserFeedMapping {
-  @PrimaryColumn({ type: 'int', name: 'userId' })
+  @PrimaryColumn({ type: 'int' })
+  userId: number;
+
+  @PrimaryColumn({ type: 'int' })
+  feedId: number;
+
   @ManyToOne((type) => User, (user) => user.feeds)
   user: User;
 
-  @PrimaryColumn({ type: 'int', name: 'feedId' })
   @ManyToOne((type) => Feed)
   feed: Feed;
 
