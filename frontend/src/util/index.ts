dafceec9874a4d776c7661449dfa24994a03ed21
GyuhyunBaek <edhz8888@gmail.com>
2022-11-23 15:00:23 +0900

    refactor : useRef와 Input을 바인딩하는 onChange 함수의 명칭 변경

--- frontend/src/util/index.ts
+++ frontend/src/util/index.ts
@@ -1,4 +1,4 @@
-export const onChange = (e: React.ChangeEvent<HTMLInputElement>, type: React.MutableRefObject<string>) => {
+export const bindValue = (e: React.ChangeEvent<HTMLInputElement>, type: React.MutableRefObject<string>) => {
   const value = e.target.value
   type.current = value
 }
