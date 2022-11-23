f427c5087a1b84d7dea5144438f3b4881917cd6a
GyuhyunBaek <edhz8888@gmail.com>
2022-11-23 14:59:45 +0900

    refactor : 닉네임 입력받는 부분을 Input 컴포넌트를 사용하도록 수정

--- frontend/src/pages/Info/index.tsx
+++ frontend/src/pages/Info/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import './style.scss'
 import XoxoIcon from '@assets/xoxoIcon.svg'
-import { onChange } from '@src/util'
+import Input from '@components/Input'
 
 const Info = () => {
   const userNickname = useRef('')
@@ -9,18 +9,15 @@ const Info = () => {
     <div className="signin-page">
       <div className="signin-body">
         <img className="icon-image" src={XoxoIcon} alt="serviceIcon" />
-        <div className="form-wrapper">
-          <label className="form-label" htmlFor="userNickname">
-            닉네임
-          </label>
-          <input
-            type="text"
-            id="userNickname"
-            placeholder="  닉네임을 입력해주세요. ( 조합 10자리 이하)  "
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e, userNickname)}
-          />
-          <button className="form-button">시작하기</button>
-        </div>
+        <Input
+          label="닉네임"
+          placeholder="닉네임을 입력해주세요. ( 조합 10자리 이하)"
+          bind={userNickname}
+          validate={(str) => {
+            return str.length > 10 ? '10자이상입니다.' : ''
+          }}
+        />
+        <button className="form-button">시작하기</button>
       </div>
     </div>
   )
