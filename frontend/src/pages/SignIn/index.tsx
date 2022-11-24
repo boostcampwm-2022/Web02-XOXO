48605fb9a1e309eb1d33663437779a633a5245d7
GyuhyunBaek <edhz8888@gmail.com>
2022-11-24 17:47:58 +0900

    refactor : 컴포넌트로 분리한 배경을 signin,info 페이지에 반영

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -4,33 +4,36 @@ import './style.scss'
 import XoxoIcon from '@assets/xoxoIcon.svg'
 import { ReactComponent as KakaoIcon } from '@assets/kakaoIcon.svg'
 import { ReactComponent as CameraIcon } from '@assets/cameraIcon.svg'
+import SigninBackground from '@src/components/SigninBackground'
 
 const SignIn = () => {
   const KAKAO_CLIENT_ID = String(process.env.REACT_APP_KAKAO_CLIENT_ID)
   const KAKAO_REDIRECT_URI = String(process.env.REACT_APP_KAKAO_REDIRECT_URI)
   return (
     <div className="signin-page">
-        <div className='signin-body'>
-          <div className="xoxo-info">
-            <span>
-              <CameraIcon/>
-              <p/>
-              my feed, our memory!
-              <p/>
-              XOXO를 통해 우리만의 추억을 공유해보세요
-            </span>
-
-          </div>
-          <img className="icon-image" src={XoxoIcon} alt="test" />
-          <a href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}`}>
-            <button className='kakao-auth-button'>
-              <div>
-                  <KakaoIcon/>
-                  <span>카카오 로그인</span>
-              </div>
-            </button>
-        </a>
+      <SigninBackground />
+      <div className="signin-body">
+        <div className="xoxo-info">
+          <span>
+            <CameraIcon />
+            <p />
+            my feed, our memory!
+            <p />
+            XOXO를 통해 우리만의 추억을 공유해보세요
+          </span>
         </div>
+        <img className="icon-image" src={XoxoIcon} alt="test" />
+        <a
+          href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}`}
+        >
+          <button className="kakao-auth-button">
+            <div>
+              <KakaoIcon />
+              <span>카카오 로그인</span>
+            </div>
+          </button>
+        </a>
+      </div>
     </div>
   )
 }
