07f8e5616520de8c6be6a4403bce4f3544bcb1e7
CHOIHYEONSEO <54612959+CHOIHYEONSEO@users.noreply.github.com>
2022-11-24 18:34:44 +0900

    fix: 충돌 해결
    충돌 해결을 위한 디렉토리 삭제

--- backend/src/feed/dto/create.feed.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { Feed } from 'src/entities/Feed.entity';
-
-export default class CreateFeedDto extends PickType(Feed, [
-  'name',
-  'thumbnail',
-  'description',
-  'dueDate',
-] as const) {}
