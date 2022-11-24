7964ee48fdbeacc1ad4da01ecedc85f3548f7044
CHOIHYEONSEO <dwc06631@gmail.com>
2022-11-24 18:54:35 +0900

    fix: 충돌 해결

---
+++ backend/src/customValidators/nicknameValidate.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+  ValidationOptions,
+  registerDecorator,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'InvalideNickname' })
+@Injectable()
+export class InvalidNickname implements ValidatorConstraintInterface {
+  async validate(nickname: string, args: ValidationArguments) {
+    if (!nickname) return false;
+    return nickname.length < 10;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'InvalidNickname';
+  }
+}
+
+export default function IsValidNickname(validationOptions?: ValidationOptions) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'IsValidNickname',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: InvalidNickname,
+    });
+  };
+}
