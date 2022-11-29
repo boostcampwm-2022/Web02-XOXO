daa87f8b263fc5702cfbbd419e2d26ccd515a8ff
jungmin <batay@soongsil.ac.kr>
2022-11-29 19:25:01 +0900

    feat : write 페이지 오브젝트 스토리지 업로드 기능 구현

--- frontend/src/pages/Write/index.tsx
+++ frontend/src/pages/Write/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from 'react'
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import React, { useState, useRef, ChangeEventHandler, useEffect } from 'react'
 import { toast } from 'react-toastify'
 import Toast from '@src/components/Toast'
 import Header from '@components/Header'
@@ -6,42 +7,64 @@ import './style.scss'
 import { ReactComponent as CameraIcon } from '@assets/uploadCameraIcon.svg'
 import { ReactComponent as XIcon } from '@assets/XIcon.svg'
 import ThumbnailPreview from './ThumbnailPreview'
+// import uploader from '@util/uploader'
+import usePost from '@hooks/usePost'
 
 const Write = () => {
+  const [imageList, setImageList] = useState<File[]>([])
   const [imagePreviews, setImagePreviews] = useState<string[]>([])
   const [isModalOpen, setModalOpen] = useState(false)
   const inputRef = useRef<HTMLInputElement | null>(null)
-  /**
-   * input으로 받은 이미지 객체를 url로 바꾸어 주는 함수입니다.
-   * @param event 이미지를 input으로 받은 event 객체
-   */
-  const onChangeImagePreviews = (event: any) => {
-    const imageLists = event.target.files
-    const imageUrlLists = [...imagePreviews]
-    if (Number(imageLists.length) + Number(imageUrlLists.length) > 10) {
+  const postImage = usePost('/image')
+
+  useEffect(() => {
+    const imageUrlList = Object.values(imageList).map((image: any) => {
+      return URL.createObjectURL(image)
+    })
+    setImagePreviews(imageUrlList)
+  }, [imageList])
+
+  const handleImageState: ChangeEventHandler<HTMLInputElement> = (event: any) => {
+    const images: FileList = event.target.files
+    if (Number(imageList.length) + Number(images.length) > 10) {
       toast('이미지는 최대 10장만 업로드 가능합니다.')
       return
     }
-    Object.values(imageLists).forEach((image: any) => {
-      imageUrlLists.push(URL.createObjectURL(image))
-    })
-
-    setImagePreviews(imageUrlLists)
+    setImageList(prev => [...prev, ...Array.from(images)])
   }
-  const onClickCameraIcon = () => {
+
+  const handleCameraButton = () => {
     if (inputRef.current !== null) {
       inputRef.current.click()
     }
   }
-  const onClickDeleteButton = (id: number) => {
+  const handleDeleteButton = (id: number) => {
     setImagePreviews(imagePreviews.filter((_, index) => index !== id))
+    setImageList(imageList.filter((_, index) => index !== id))
   }
-  const onClickThumbnailModal = (e: any) => {
+  const handleThumbnailModal = (e: any) => {
     e.stopPropagation()
     setModalOpen(true)
   }
   const isDisabledButton = () => {
     return imagePreviews.length === 0
+  }
+  const imageUpload = async () => {
+    const formData = new FormData()
+    imageList.forEach((image) => {
+      console.log(image)
+      formData.append('image', image)
+    })
+    const response = await postImage(formData)
+    if (response !== undefined) return response.data
+  }
+  const handleUploadPosting = async () => {
+    const imagePathList = await imageUpload()
+    console.log(imagePathList)
+    setImagePreviews([]) // TODO 추후 삭제
+    setImageList([]) // TODO 추후 삭제
+    // TODO 이 이미지 주소 리스트와 내용을 모두 api로 전달한다.
+    // TODO 정상적으로 글쓰기가 완료되면, 피드페이지로 리다이렉트 시킨다.
   }
 
   return (
@@ -60,19 +83,19 @@ const Write = () => {
                           <div>
                             <img className='image-holder' src={image} alt="이미지" />
                           </div>
-                          <div className="image-delete-button" onClick={() => onClickDeleteButton(id)}>
+                          <div className="image-delete-button" onClick={() => handleDeleteButton(id)}>
                               <XIcon width="2.5vw" height="3.75vw" fill="#ffffff"/>
                           </div>
                         </div>
                     ))}
                 </div>
                 <div className="button-bar-wrapper">
-                  <CameraIcon onClick={onClickCameraIcon}/>
-                  <input type="file" multiple className="image-input" accept='image/*' ref={inputRef} onChange={onChangeImagePreviews}/>
-                  <button className='thumbnail-preview' onClick={onClickThumbnailModal} disabled={isDisabledButton()}>썸네일 미리보기</button>
+                  <CameraIcon onClick={handleCameraButton}/>
+                  <input type="file" multiple className="image-input" accept='image/*' ref={inputRef} onChange={handleImageState}/>
+                  <button className='thumbnail-preview' onClick={handleThumbnailModal} disabled={isDisabledButton()}>썸네일 미리보기</button>
                 </div>
                 <textarea className='text-area' placeholder='글 내용을 입력해주세요'/>
-              <button className="write-button" disabled={isDisabledButton()}>게시물 업로드 하기</button>
+              <button className="write-button" disabled={isDisabledButton()} onClick={handleUploadPosting}>게시물 업로드 하기</button>
               <Toast />
             </div>
         </div>
