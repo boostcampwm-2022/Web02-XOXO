5c0b3138f996166e5844b0e443f8f30df4c4b291
GyuhyunBaek <edhz8888@gmail.com>
2022-12-06 13:32:41 +0900

    fix : fetcher,usePost에서 리턴해주는 방식 변경

--- frontend/src/hooks/usePost.tsx
+++ frontend/src/hooks/usePost.tsx
@@ -1,18 +1,18 @@
 import { useCallback } from 'react'
 import axios from 'axios'
-import { isEmpty } from 'lodash'
 
 const usePost = (url: string) => {
-  const post = useCallback(async (body: object, options?: object) => {
-    try {
-      const response = await axios.post(url, body, { ...options, withCredentials: true })
-      const { status, data, success } = response.data
-      if (isEmpty(success)) console.log(status)
-      return data
-    } catch (err) {
-      console.log(err)
-    }
-  }, [url])
+  const post = useCallback(
+    async (body: object, options?: object) => {
+      try {
+        const response = await axios.post(url, body, { ...options, withCredentials: true })
+        return response.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    [url]
+  )
 
   return post
 }
--- frontend/src/util/fetcher.ts
+++ frontend/src/util/fetcher.ts
@@ -1,11 +1,8 @@
 import axios from 'axios'
-import { isEmpty } from 'lodash'
 
 const fetcher = async (url: string) => {
   const response = await axios.get(url)
-  const { success, status, data } = response.data
-  if (isEmpty(success)) console.log(status)
-  return data
+  return response.data
 }
 
 export default fetcher
