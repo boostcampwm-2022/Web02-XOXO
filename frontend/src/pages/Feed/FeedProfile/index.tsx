16ecd92dcaafd1175176f299146b19ffdd4917ef
jungmin <batay@soongsil.ac.kr>
2022-12-08 04:17:07 +1400

    feat : 피드 프로필 정보 렌더링 구현

--- frontend/src/pages/Feed/FeedProfile/index.tsx
+++ frontend/src/pages/Feed/FeedProfile/index.tsx
@@ -1,16 +1,27 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import React, { useEffect } from 'react'
 import './style.scss'
 import { ReactComponent as ShareIcon } from '@assets/shareIcon.svg'
 import { ReactComponent as EditIcon } from '@assets/editIcon.svg'
 import DefaultUserImage from '@assets/defaultUserImage.svg'
+import { remainDueDate } from '@util/index'
 
-const FeedProfile = () => {
-  // 테스트 데이터
-  const feedInfo = { name: '테스트', image: '', description: '가시밭길 위로 riding, you made me boost up (ah-ah-ah-ah) 거짓으로 가득 찬 party 가렵지도 않아 내 뒤에 말들이 많아', postingNum: 15, remainTime: '14일 4시간' }
+interface IProps {
+  thumbnail: string
+  description: string
+  dueDate: string
+  isOwner: boolean
+  isGroupFeed: boolean
+  postingCnt: number
+}
+
+const FeedProfile = ({ thumbnail, description, dueDate, postingCnt, isOwner, isGroupFeed }: IProps) => {
   const kakaoJavascriptKey = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY
   const kakaoMessageTemplate = Number(process.env.REACT_APP_KAKAO_MESSAGE_TEMPLATE)
   useEffect(() => {
-    window.Kakao.init(kakaoJavascriptKey)
+    if (!(window.Kakao.isInitialized())) {
+      window.Kakao.init(kakaoJavascriptKey)
+    }
   }, [])
   const sendKakaoMessage = () => {
     window.Kakao.Share.sendCustom({
@@ -22,7 +33,7 @@ const FeedProfile = () => {
         <div className="feed-profile-header">
             <div className="feed-profile-image-wrapper">
                 <div className="feed-profile-image">
-                    <img src={feedInfo.image !== '' ? feedInfo.image : DefaultUserImage} alt="유저 프로필 이미지"/>
+                    <img src={thumbnail !== '' ? thumbnail : DefaultUserImage} alt="유저 프로필 이미지"/>
                 </div>
                 <div className="edit-button">
                     <EditIcon/>
@@ -30,21 +41,23 @@ const FeedProfile = () => {
             </div>
             <div className="feed-profile-info-wrapper">
                 <span className="feed-profile-description">
-                    {feedInfo.description}
+                    {description}
                 </span>
                 <div className="feed-profile-status">
-                    <span className="feed-post-number">게시물 수 <span className="bold">{feedInfo.postingNum}</span></span>
+                    <span className="feed-post-number">게시물 수 <span className="bold">{postingCnt}</span></span>
                     <span className="line"></span>
-                    <span className="feed-remaining-time">남은 시간 <span className="bold">{feedInfo.remainTime}</span></span>
+                    <span className="feed-remaining-time">남은 시간 <span className="bold">{remainDueDate(dueDate)}</span></span>
                 </div>
             </div>
         </div>
-        <button className='feed-share-button' onClick={sendKakaoMessage}>
+        {
+          !isGroupFeed && isOwner && <button className='feed-share-button' onClick={sendKakaoMessage}>
             <div>
                 <ShareIcon/>
                 <span>내 피드 공유하기</span>
             </div>
-        </button>
+          </button>
+        }
     </div>
   )
 }
