640a0508e865d97598e25a337180f9b6dee42977
CHOIHYEONSEO <dwc06631@gmail.com>
2022-12-07 12:21:02 +0900

    refactor: 피드 주인 여부 내려줌

--- backend/src/feed/feed.service.ts
+++ backend/src/feed/feed.service.ts
@@ -24,14 +24,15 @@ export class FeedService {
     private dataSource: DataSource,
   ) {}
 
-  async getFeedById(encryptedFeedID: string) {
+  async getFeedInfo(encryptedFeedID: string, userId: number) {
     try {
       const id = Number(decrypt(encryptedFeedID));
       const feed = await this.dataSource.getRepository(Feed).find({
         where: { id },
-        relations: ['postings'],
+        relations: ['postings', 'users'],
         select: {
           postings: { id: true },
+          users: { userId: true },
           name: true,
           description: true,
           thumbnail: true,
@@ -39,8 +40,7 @@ export class FeedService {
         },
       });
 
-      if (!feed) throw new NonExistFeedError();
-      return new FeedInfoDto(feed[0]);
+      return FeedInfoDto.createFeedInfoDto(feed[0], userId);
     } catch (e) {
       if (
         e instanceof NonExistFeedError ||
