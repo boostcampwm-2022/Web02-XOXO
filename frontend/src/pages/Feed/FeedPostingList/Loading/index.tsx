95ec747f9d2113d134e74c3b83c688c2c3c687f6
jungmin <batay@soongsil.ac.kr>
2022-12-08 04:14:33 +1400

    feat : 무한 스크롤 때 로딩 컴포넌트 구현

---
+++ frontend/src/pages/Feed/FeedPostingList/Loading/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.scss'
+
+const Loading = () => {
+  return (
+  <div className="loading">
+    <div className="loading-circle">
+      <div className="loading-empty"></div>
+    </div>
+  </div>
+  )
+}
+
+export default Loading
