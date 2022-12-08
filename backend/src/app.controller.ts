baeac4642e6c856e3b0892ccb81b7028b70ce5d5
CHOIHYEONSEO <dwc06631@gmail.com>
2022-12-09 01:23:56 +0900

    feat: 현재 서버 시간 내려주는 api 생성

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import AppService from './app.service';
+import { ResponseEntity } from './common/response/response.entity';
 
 @Controller()
 export default class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('serverTime')
+  getServerTime() {
+    return ResponseEntity.OK_WITH_DATA(Date.now());
   }
 }
