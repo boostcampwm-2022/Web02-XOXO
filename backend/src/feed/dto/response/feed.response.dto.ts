478d5c6cbeedbbeadd0680509c4eacb71887c619
CHOIHYEONSEO <dwc06631@gmail.com>
2022-12-08 21:07:48 +0900

    refactor: 개인 피드 리스트, 그룹 피드 리스트를 조회할 시 암호화 된 스트링을 보낸다

--- backend/src/feed/dto/response/feed.response.dto.ts
+++ backend/src/feed/dto/response/feed.response.dto.ts
@@ -9,22 +9,19 @@ export default class FeedResponseDto extends PickType(Feed, [
 ] as const) {
   encryptedId: string;
 
-  constructor(feed: Feed, isGroupFeed: boolean) {
+  constructor(feed: Feed) {
     super();
     this.name = feed.name;
     this.thumbnail = feed.thumbnail;
     this.encryptedId = encrypt(feed.id.toString());
-    this.isGroupFeed = isGroupFeed;
   }
 
-  static makeFeedResponseDto(feed: Feed, isGroupFeed: boolean) {
-    return new FeedResponseDto(feed, isGroupFeed);
+  static makeFeedResponseDto(feed: Feed) {
+    return new FeedResponseDto(feed);
   }
 
-  static makeFeedResponseArray(feeds: Feed[], isGroupFeed: boolean) {
-    const feedResponseArray = feeds.map(
-      (feed) => new FeedResponseDto(feed, isGroupFeed),
-    );
+  static makeFeedResponseArray(feeds: Feed[]) {
+    const feedResponseArray = feeds.map((feed) => new FeedResponseDto(feed));
     return feedResponseArray;
   }
 }
