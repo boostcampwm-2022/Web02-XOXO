2cb0acee898e829823f61fb68d938f28d52182a7
GyuhyunBaek <edhz8888@gmail.com>
2022-12-12 09:55:56 +0900

    refactor : 인터페이스를 createfeed 안 types.ts에서 보관하도록 변경

---
+++ frontend/src/pages/CreateFeed/types.ts
@@ -0,0 +1,18 @@
+export interface IFeedForm {
+  name?: string
+  thumbnail?: string
+  description?: string
+  dueDate?: string
+  memberIdList?: number[]
+}
+export interface ISuggestion {
+  id: string
+  nickname: string
+}
+export interface IGroupMember {
+  members: ISuggestion[]
+  setMembers: React.Dispatch<React.SetStateAction<ISuggestion[]>>
+}
+export interface ICreateFeedButton {
+  getFeedInfos: () => Promise<IFeedForm | undefined>
+}
