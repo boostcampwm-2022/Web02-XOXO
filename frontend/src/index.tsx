3cf96a158a703d518dfd59f5772ac0f03a8e5487
GyuhyunBaek <edhz8888@gmail.com>
2022-12-12 17:44:02 +0900

    refactor : 라우터 수정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import Feed from '@pages/Feed'
 import CreateFeed from '@pages/CreateFeed'
 import Write from '@pages/Write'
@@ -17,23 +17,26 @@ import './global.scss'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <>
-  <div className='image-wrapper' ><DesktopSideImage width={240}/></div>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/Feed">
-        <Route path=":feedId" element={<AuthRoute Component={Feed} />} />
-        <Route path=":feedId">
-          <Route path=":postingId" element={<AuthRoute Component={Posting} />} />
+    <div className="image-wrapper">
+      <DesktopSideImage width={240} />
+    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Signin" />} />
+        <Route path="/Feed">
+          <Route path=":feedId" element={<AuthRoute Component={Feed} />} />
+          <Route path=":feedId">
+            <Route path=":postingId" element={<AuthRoute Component={Posting} />} />
+          </Route>
         </Route>
-      </Route>
-      <Route path="/Write/:feedId" element={<AuthRoute Component={Write} />} />
-      <Route path="/SignIn/Info" element={<SigninRoute Component={Info} />} />
-      <Route path="/SignIn" element={<SigninRoute Component={SignIn} />} />
-      <Route path="/Createfeed/:path" element={<AuthRoute Component={CreateFeed} />} />
-      <Route path="/Feeds" element={<AuthRoute Component={Feeds} />} />
-      <Route path="/Posting" element={<Posting />} />
-      <Route path="/*" element={<Error />} />
-    </Routes>
-  </BrowserRouter>
+        <Route path="/Write/:feedId" element={<AuthRoute Component={Write} />} />
+        <Route path="/SignIn/Info" element={<SigninRoute Component={Info} />} />
+        <Route path="/SignIn" element={<SigninRoute Component={SignIn} />} />
+        <Route path="/Createfeed/:path" element={<AuthRoute Component={CreateFeed} />} />
+        <Route path="/Feeds" element={<AuthRoute Component={Feeds} />} />
+        <Route path="/Posting" element={<Posting />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   </>
 )
