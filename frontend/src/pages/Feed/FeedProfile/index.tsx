9b0dd18ee31e54a27348e8e567becf85142e3e0b
jungmin <batay@soongsil.ac.kr>
2022-12-13 14:17:07 +0900

    hot fix : kakao init 오류 수정 및 공유하기 오류 수정 및 image cdn 경로로 수정

--- frontend/src/pages/Feed/FeedProfile/index.tsx
+++ frontend/src/pages/Feed/FeedProfile/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import useSWR from 'swr'
 import './style.scss'
 import { ReactComponent as ShareIcon } from '@assets/shareIcon.svg'
-import { ReactComponent as EditIcon } from '@assets/editIcon.svg'
 import DefaultUserImage from '@assets/defaultUserImage.svg'
+import getThumbUrl from '@pages/Feed/imageQuery'
 import { remainDueDate } from '@util/index'
 import fetcher from '@util/fetcher'
 
@@ -19,16 +20,20 @@ interface IProps {
 
 const FeedProfile = ({ thumbnail, description, dueDate, postingCnt, isOwner, isGroupFeed }: IProps) => {
   const { data: serverDate } = useSWR('/serverTime', fetcher)
+  const { feedId } = useParams<{ feedId: string }>()
   const kakaoJavascriptKey = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY
   const kakaoMessageTemplate = Number(process.env.REACT_APP_KAKAO_MESSAGE_TEMPLATE)
   useEffect(() => {
     if (!(window.Kakao.isInitialized())) {
       window.Kakao.init(kakaoJavascriptKey)
     }
-  }, [])
+  }, [kakaoJavascriptKey])
   const sendKakaoMessage = () => {
     window.Kakao.Share.sendCustom({
-      templateId: kakaoMessageTemplate
+      templateId: kakaoMessageTemplate,
+      templateArgs: {
+        feedUrl: `Feed/${feedId}`
+      }
     })
   }
   return (
@@ -36,10 +41,7 @@ const FeedProfile = ({ thumbnail, description, dueDate, postingCnt, isOwner, isG
         <div className="feed-profile-header">
             <div className="feed-profile-image-wrapper">
                 <div className="feed-profile-image">
-                    <img src={thumbnail !== '' ? thumbnail : DefaultUserImage} alt="유저 프로필 이미지"/>
-                </div>
-                <div className="edit-button">
-                    <EditIcon/>
+                    <img src={thumbnail !== '' ? getThumbUrl(thumbnail) : DefaultUserImage} alt="유저 프로필 이미지"/>
                 </div>
             </div>
             <div className="feed-profile-info-wrapper">
