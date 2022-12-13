f68d4d26d0675e44171942766b9ae3b3d4b9ae87
jungmin <batay@soongsil.ac.kr>
2022-12-13 18:21:18 +0900

    feat : 이미지 변환 룰 util 구현

---
+++ frontend/src/util/imageQuery.ts
@@ -0,0 +1,48 @@
+
+const getThumbSize = (type: string, windowWidth: number) => {
+  const level = Math.floor(windowWidth / 100)
+  switch (type) {
+    case 'postingLQ' :
+      if (level >= 5) return 50
+      if (level >= 4) return 40
+      return 30
+    case 'posting' :
+      if (level >= 5) return 500
+      if (level >= 4) return 400
+      return 300
+    case 'feed' :
+      if (level >= 4) return 240
+      if (level >= 3) return 180
+      return 120
+    case 'feeds' :
+      if (level >= 4) return 120
+      if (level >= 3) return 90
+      return 40
+    default :
+      return 300
+  }
+}
+
+const getQueryString = (type: string) => {
+  const windowWidth = window.innerWidth
+  const size = getThumbSize(type, windowWidth)
+  return `?type=m&w=${size}&h=${size}`
+}
+
+const getPostingLQThumbUrl = (url: string) => {
+  return `${process.env.REACT_APP_CDN_URI as string}/` + url + `${getQueryString('postingLQ')}`
+}
+
+const getPostingThumbUrl = (url: string) => {
+  return `${process.env.REACT_APP_CDN_URI as string}/` + url + `${getQueryString('posting')}`
+}
+
+const getFeedThumbUrl = (url: string) => {
+  return `${process.env.REACT_APP_CDN_URI as string}/` + url + `${getQueryString('feed')}`
+}
+
+const getFeedsThumbUrl = (url: string) => {
+  return `${process.env.REACT_APP_CDN_URI as string}/` + url + `${getQueryString('feeds')}`
+}
+
+export { getPostingLQThumbUrl, getPostingThumbUrl, getFeedsThumbUrl, getFeedThumbUrl }
