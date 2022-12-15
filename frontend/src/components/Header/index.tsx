5cecd1fa32f3c35aa5cf81fec37b274ec99a59f7
GyuhyunBaek <edhz8888@gmail.com>
2022-12-15 13:29:19 +0900

    feat : 헤더에 로그아웃버튼, 모달버튼 추가, 스타일링 일부 수정

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import React from 'react'
+import React, { useState } from 'react'
 import './style.scss'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as DownIcon } from '@assets/downIcon.svg'
 import { ReactComponent as LogoutIcon } from '@assets/logoutIcon.svg'
+import { ReactComponent as QuestionIcon } from '@assets/questionIcon.svg'
 import usePost from '@hooks/usePost'
 import { IResponse } from '@src/types'
 import useSWR from 'swr'
+import InfoModal from '../InfoModal'
 
 interface headerProps {
   page?: string
@@ -16,6 +18,7 @@ interface headerProps {
 
 const Header = ({ page, text }: headerProps) => {
   const postLogout = usePost('/users/logout')
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const { data: nickname } = useSWR('/users/nickname', () => {
     return '하드코딩된닉네임'
   })
@@ -24,6 +27,10 @@ const Header = ({ page, text }: headerProps) => {
     const { data }: IResponse = await postLogout({})
 
     if (data) navigate('/Signin')
+  }
+  const handleModalOpen = (e: any) => {
+    e.stopPropagation()
+    setIsModalOpen(true)
   }
   const handleRenderHeader = () => {
     switch (page) {
@@ -44,10 +51,28 @@ const Header = ({ page, text }: headerProps) => {
           </div>
         )
       default:
-        return <div className="default-header">{text}</div>
+        return (
+          <div className="default-header">
+            <button onClick={handleModalOpen}>
+              <div className="svg-wrapper">
+                <QuestionIcon />
+              </div>
+            </button>
+            <span>{text}</span>
+            <button onClick={handleLogout}>
+              <div className="svg-wrapper">
+                <LogoutIcon />
+              </div>
+            </button>
+          </div>
+        )
     }
   }
-  return <>{handleRenderHeader()}</>
+  return (
+    <>
+      {handleRenderHeader()} {isModalOpen && <InfoModal isModalOpen={isModalOpen} setModalOpen={setIsModalOpen} />}
+    </>
+  )
 }
 
-export default React.memo(Header)
+export default Header
