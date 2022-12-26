8c3bb297d9a3747c40d8895e1b28d8fd49a16a55
jungmin <batay@soongsil.ac.kr>
2022-12-27 04:27:54 +0900

    feat:피드 수정 페이지 route 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import Feed from '@pages/Feed'
 import CreateFeed from '@pages/CreateFeed'
+import EditFeed from '@pages/EditFeed'
 import Write from '@pages/Write'
 import SignIn from '@pages/SignIn'
 import Posting from '@pages/Posting'
@@ -30,6 +31,7 @@ root.render(
         <Route path="/SignIn/Info" element={<SigninRoute Component={Info} />} />
         <Route path="/SignIn" element={<SigninRoute Component={SignIn} />} />
         <Route path="/Createfeed/:path" element={<AuthRoute Component={CreateFeed} />} />
+        <Route path="/EditFeed/:path/:feedId" element={<AuthRoute Component={EditFeed} />} />
         <Route path="/Feeds" element={<AuthRoute Component={Feeds} />} />
         <Route path="/Posting" element={<Posting />} />
         <Route path="/*" element={<Error />} />
